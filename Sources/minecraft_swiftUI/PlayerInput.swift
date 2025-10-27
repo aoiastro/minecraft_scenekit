ec22a12d004940447ba50c1a26533f266d5b19d7
aoiastro <ninjin0925@icloud.com>
2025-10-27 11:13:52 +0900

    Delete Sources/minecraft_swiftUI/PlayerInput.swift

--- Sources/minecraft_swiftUI/PlayerInput.swift
+++
@@ -1,9 +0,0 @@
-
-import Foundation
-import Combine
-import CoreGraphics // For CGPoint
-
-class PlayerInput: ObservableObject {
-    @Published var moveDirection: CGPoint = .zero // x, y for joystick
-    @Published var jumpPressed: Bool = false
-}
